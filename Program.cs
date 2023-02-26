d3c1b1a59ac9e827db7aaaf2d83ee1c68cb872e9
NodirbekMir <122109038+NodirbekMir@users.noreply.github.com>
2023-02-26 21:17:49 +0600

    gkkgg,

--- Program.cs
+++ Program.cs
@@ -11,3 +11,40 @@ void New(int N)
     }
 }
 New(5);
+/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+int n, m, s=0;
+Console.Write("Введи значение от: ");
+n = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи значение до: ");
+m = Convert.ToInt16(Console.ReadLine());
+for (int i = n; i <= m; i++)
+    s += i;
+Console.WriteLine(s);
+/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
+int[,] Create2DRandomArray()
+                    static int Ack(int m, int n)
+        {
+            if (m == 0)
+            {
+                return n + 1;
+            }else if (n == 0)
+            {
+                return Ack(m - 1, 1);
+            }else
+            {
+                return Ack(m - 1, Ack(m, n - 1));
+            }
+ 
+        }
+ 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            int eee = Ack(a, b);
+           textBox3.Text = eee.ToString();
+        }
