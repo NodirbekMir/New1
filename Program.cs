393009a2d8c7fa1d8750722dd4c8b62d9656e917
Nodirbek <mirzajanovnodirbek@gmail.com>
2023-02-26 21:26:06 +0600

    kfjbjg

--- Program.cs
+++ Program.cs
@@ -25,26 +25,18 @@ Console.WriteLine(s);
 /*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29*/
-int[,] Create2DRandomArray()
-                    static int Ack(int m, int n)
-        {
-            if (m == 0)
-            {
-                return n + 1;
-            }else if (n == 0)
-            {
-                return Ack(m - 1, 1);
-            }else
-            {
-                return Ack(m - 1, Ack(m, n - 1));
-            }
- 
-        }
- 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            int a = int.Parse(textBox1.Text);
-            int b = int.Parse(textBox2.Text);
-            int eee = Ack(a, b);
-           textBox3.Text = eee.ToString();
-        }
+int AkkermanFunc(int m, int n)
+		{
+			if(m == 0)
+			{
+				return n + 1;
+			}
+			if(m > 0 && n == 0)
+			{
+				return AkkermanFunc(m - 1, 1);
+			}
+			return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+		}
+		
+		Console.WriteLine(AkkermanFunc(3,2));
+	
